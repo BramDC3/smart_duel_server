b694c39b68d9b67645fd5762f475987b662f3fdf
Bram De Coninck <bram.deconinck@inthepocket.com>
2021-02-02 21:00:59 +0100

    Start using TypeScript instead of JavaScript.

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
-const server = require('http').createServer();
-const io = require('socket.io')(server);
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var server = require('http').createServer();
+var io = require('socket.io')(server);
 io.on('connection', function (socket) {
-
     console.log('socket connect...', socket.id);
-
-    socket.on('summonEvent', function name(data) {
+    socket.on('summonEvent', function (data) {
         console.log(data);
         socket.broadcast.emit('summonEvent', data);
     });
-
-    socket.on('connect', function () {
-    });
-
     socket.on('disconnect', function () {
         console.log('socket disconnect...', socket.id);
     });
-
     socket.on('error', function (err) {
         console.log('received error from socket:', socket.id);
         console.log(err);
     });
 });
-
 var server_port = process.env.PORT || 3000;
 server.listen(server_port, function (err) {
-    if (err) throw err;
+    if (err)
+        throw err;
     console.log('Listening on port %d', server_port);
 });
---
+++ index.ts
@@ -0,0 +1,28 @@
+import { Socket } from "socket.io";
+
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+
+io.on('connection', (socket: Socket) => {
+    console.log('socket connect...', socket.id);
+
+    socket.on('summonEvent', (data) => {
+        console.log(data);
+        socket.broadcast.emit('summonEvent', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('socket disconnect...', socket.id);
+    });
+
+    socket.on('error', (err) => {
+        console.log('received error from socket:', socket.id);
+        console.log(err);
+    });
+});
+
+const server_port = process.env.PORT || 3000;
+server.listen(server_port, function (err: any) {
+    if (err) throw err;
+    console.log('Listening on port %d', server_port);
+});
